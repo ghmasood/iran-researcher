20c696cbe1f92eff18c2bcbc7c3d6fc0627764f7
ghmasood <msd.drg@gmail.com>
2023-06-17 19:58:55 +0330

    [BUGFIX] logo position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LogoType from "components/logos/logoType";
 
 export default function Home() {
   return (
-    <div className="w-full flex justify-center">
-      <LogoType className="w-[60vw] brightness-0 invert" />
+    <div className="w-full flex justify-center items-center gap-[3rem] flex-col">
+      <LogoType className="w-[60vw] h-auto brightness-0 invert mt-[5rem]" />
     </div>
   );
 }
