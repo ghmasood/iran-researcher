b92094ec34fde31b9e362581a7417ada545e0581
ghmasood <msd.drg@gmail.com>
2023-06-17 09:58:37 +0330

    [IMPROVE] home page for coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import LogoType from "components/logos/logoType";
 
 export default function Home() {
   return (
-    <div
-      className="font-[900] text-[12rem] text-white mt-60 text-center"
-      style={{ fontVariationSettings: "'DOTQ'1" }}
-    >
-      ایران ریسرچر
+    <div className="w-full flex justify-center">
+      <LogoType className="w-[60vw] brightness-0 invert" />
     </div>
   );
 }
