2e5657d1df9970d0422440e4467730e933ef1a4b
gh_masoud <msd.drg@gmail.com>
2023-06-17 16:31:36 +0330

    [ADD] some text for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="w-full flex justify-center">
       <LogoType className="w-[60vw] brightness-0 invert" />
+      <span>DEV SERVER</span>
     </div>
   );
 }
